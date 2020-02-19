9086e3c345f65517b31e49277a1decf374ad60fd
Brian D. Caruso <bdc34@cornell.edu>
2020-02-19 09:29:42 -0500

    Adding tests for HTML escaping inside of math and outside of math
    Inside math escaping is handled by KaTeX.
    Outside of math escaping is handled by the normal React rendering.

--- test/index.js
+++ test/index.js
@@ -29,25 +29,44 @@ describe("react-latex", () => {
     it("Should have katex class", () => {
         const testString = ReactDomServer.renderToStaticMarkup(<Test />);
         assert.notEqual(-1, testString.indexOf("<span class=\"katex-mathml\">"));
-        assert.equal('a', testString);
     });
 
     // it("Should render string with brackets properly", () => {
     //     const testStringWithBrackets = ReactDomServer.renderToStaticMarkup(<TestBrackets />).trim();
     //     assert.equal(bracketString, testStringWithBrackets);
     // });
-});
 
-
-class TestLtGt extends React.Component {
-    render () {
-        return React.createElement(latex, null,  "compact size (< 20'), spectral types >M4");
-    }
-}
-
-describe("react-latex2", () => {
-    it("It should not mess up gt and lt", () => {
+    it("It should not strip gt and lt", () => {        
+        class TestLtGt extends React.Component {
+            render () {
+                return React.createElement(latex, null,  "compact size (< 20'), spectral types >M4");
+            }
+        }
         const testString = ReactDomServer.renderToStaticMarkup(<TestLtGt />);
         assert.equal( testString, "<span>compact size (&lt; 20&#x27;), spectral types &gt;M4</span>");
     });
+
+    it("It should escape HTML tags outside of math", () => {        
+        class TestLtGt extends React.Component {
+            render () {
+                return React.createElement(latex, null,
+                    "compact size (<script>window.bad();</script> 20'), spectral $\\sqrt{8}$ types >M4");
+            }
+        }
+        const testString = ReactDomServer.renderToStaticMarkup(<TestLtGt />);
+        assert.equal(-1,  testString.indexOf("<script>window.bad();</script>"));
+    });
+    
+    it("It should escape HTML tags inside of math", () => {        
+        class TestLtGt extends React.Component {
+            render () {
+                return React.createElement(latex, null,  "compact size $<script>window.dobadstuff()</script>$");
+            }
+        }
+        const testString = ReactDomServer.renderToStaticMarkup(<TestLtGt />);
+        assert.equal(-1,  testString.indexOf("<script>window.dobadstuff()</script>"));
+    });
+
+    
 });
+
